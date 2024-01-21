18f548a5ca6fdf0fca380d619f00d139c5fc2403
ctrevai <ctrevai@gmail.com>
2024-01-21 14:13:15 +0800

    initial commit

--- docker_lambda/docker_lambda_stack.py
+++ docker_lambda/docker_lambda_stack.py
@@ -1,14 +1,36 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
+    CfnOutput,
+    aws_lambda as lambda_,
     # aws_sqs as sqs,
 )
 from constructs import Construct
+
 
 class DockerLambdaStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        dockerFunc = lambda_.DockerImageFunction(
+            self, "docker_lambda",
+            code=lambda_.DockerImageCode.from_image_asset("./image"),
+            memory_size=1024,
+            timeout=Duration.seconds(10),
+            architecture=lambda_.Architecture.ARM_64,
+        )
+
+        functionUrl = dockerFunc.add_function_url(
+            auth_type=lambda_.FunctionUrlAuthType.AWS_IAM,
+            cors=lambda_.FunctionUrlCorsOptions(
+                allowed_methods=[lambda_.HttpMethod.ALL],
+                allowed_headers=['*'],
+                allowed_origins=['*'],
+            )
+        )
+
+        CfnOutput(self, "FunctionUrlOutput", value=functionUrl.url)
 
         # The code that defines your stack goes here
 
---
+++ image/src/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def handler(event, context):
+    arr = np.random.randint(0, 10, (3, 3))
+    return {
+        "statusCode": 200,
+        "body":
+            {
+                "message": "Hello from DockerLambda",
+                "array": arr.tolist()
+            }
+    }
